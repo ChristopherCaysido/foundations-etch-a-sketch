cedddf78a6a3575c6063a753e5e8dd8a10c96ec3
ChristopherCaysido <christopher.caysido@gmail.com>
2025-11-18 21:23:53 +0800

    feat: created the createRow function
    createRow creates 16 squares in a row

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const mainContainer = document.querySelector("#main-container")
 function createSquare(){
     const square = document.createElement('div')
     square.classList.add('grid-square');
-    mainContainer.appendChild(square)
+    return square
 }
 
+function createRow(){
+    const row = document.createElement('div')
+    for(let i = 0; i < 16; i++){
+        const square = createSquare()
+        row.appendChild(square)
+    }
+    row.classList.add('row')
+    return row
+}
